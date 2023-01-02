e722bc1d43c26911acdd1f8281e269eeb7e69936
AlexZhebet <OleksandrZebet@gmail.com>
2023-01-02 20:32:46 +0200

    change

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const userFameli = prompt('Enter your last name'); 
+const userFameli = prompt('Enter your last name?'); 
 const userName1 = prompt('What is your name?'); 
-const userAge = +prompt('How old are you'); 
+const userAge = +prompt('How old are you?'); 
 alert(`My name ${userName1} ${userFameli} I'm ${userAge}  years old`); 
 console.log(`My name ${userName1} ${userFameli} I'm ${userAge}  years old`);
