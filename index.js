cce26ecaa0ac7dd037e1a4708d3d25e387104195
AlexZhebet <OleksandrZebet@gmail.com>
2023-01-02 19:55:18 +0200

    experience

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('homework №1');
+const userName = prompt('Як Вас звати?')
+const userAge = prompt('Скільки Вам років?')
+const userSmoge = confirm('Ви курите?')
+
+if (userAge > 18) {
+    if (userSmoges) {
+        console.log(`Привіт ${userName}, що ж, ти сам обрав цей шлях...`)
+    }
+    else {
+        console.log(`Привіт ${userName}, чудово!Ти мабуть ще й спортом займаєшся!`);
+    }
+} else if (userAge < 18) {
+    if (userSmoges) {
+        console.log(`Привіт ${userName}! А батьки знають про це?`);
+    }
+    else {
+        console.log(`Привіт ${userName}! Правильно, зараз головне добре вчитися!`);
+    }
+}
