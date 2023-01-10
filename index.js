a156460d0bed34b0e1aa076cd12d609b3982c790
AlexZhebet <115915899+AlexZhebet@users.noreply.github.com>
2023-01-10 14:13:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,28 +17,25 @@ while (numeric1 == "" || Number.isNaN(numeric1)) {
   numeric1 = +prompt(`Введіть друге число`)
 }
 
+
 let addition = (`додати`)
 let symbolAddition = (`+`)
-let resultAddition = numeric + numeric1
 let subtraction = (`відняти`)
 let symbolSubtraction = (`-`)
-let resultSubtraction = numeric - numeric1
 let multiplication = ('множення')
 let symbolMultiplication = (`*`)
-let resultMultiplication = numeric * numeric1
 let division = (`ділення`)
 let symbolDivision = (`/`)
-let resultDivision = numeric / numeric1
 
 if (resultGap === addition || resultGap === symbolAddition) {
-  alert(resultAddition)
+  alert(`${numeric} + ${numeric1} = ${numeric + numeric1}`)
 }
 if (resultGap === subtraction || resultGap === symbolSubtraction) {
-  alert(resultSubtraction)
+  alert(`${numeric} - ${numeric1} = ${numeric - numeric1}`)
 }
 if (resultGap === multiplication || resultGap === symbolMultiplication) {
-  alert(resultMultiplication)
+  alert(`${numeric} * ${numeric1} = ${numeric * numeric1}`)
 }
 if (resultGap === division || resultGap === symbolDivision) {
-  alert(resultDivision)
+  alert(`${numeric} / ${numeric1} = ${numeric / numeric1}`)
 }
